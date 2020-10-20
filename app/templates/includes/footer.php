1f562382918ee3d5f2ae8467f4d300d957c9eaf3
Tomahock <eu@tomahock.com>
2020-10-20 15:55:45 +0100

    fix footer

--- app/templates/includes/footer.php
+++ app/templates/includes/footer.php
@@ -13,7 +13,7 @@
             <div class="col-md-10 col-12 mt-5">
                 <p style="line-height: 1.7rem">Todos os contratos que aparecem nesta página, têm uma das seguintes palavras no seu texto covid, corona, SARS-COV-2, nCoV-2019, SARS, epidemia, pandemia, COV2</p>
                 <p style="line-height: 1.7rem">Essa palavra é colocada a <span class="highlight">sombreado</span></p>
-                <p style="line-height: 1.7rem">Se acha que um contrato não devia aparecer nesta página, envie um email para <a href="mailto:geral@basecovid19.pt">geral@basecovid19.pt</a> com o link desse contrato</p>
+                <p style="line-height: 1.7rem">Se acha que um contrato não devia aparecer nesta página, envie um email para <a href="mailto:geral@basecovid19.pt">geral@base-covid19.pt</a> com o link desse contrato</p>
             </div>
             <!-- Grid column -->
 
@@ -26,10 +26,18 @@
     </div>
 
     <div class="footer-copyright text-center py-3">© 2020 Copyright:
-        <a href="https://base-covid19.pt/"> BaseCOVID19</a>
-        <a class="fb-ic" href="https://twitter.com/basecovid19" target="_blank">
-            <i class="fab fa-twitter fa-lg white-text mr-4"> </i>
-        </a>
+        <a href="https://base-covid19.pt/">Base COVID19</a>
+
+    </div>
+
+    <div class="footer-copyright text-center py-3">
+        <a href="https://twitter.com/basecovid19" target="_blank"><i class="fab fa-twitter fa-lg white-text mr-4"></i></a>
+        <a href="https://github.com/tomahock/basecovid19" target="_blank"><i class="fab fa-github fa-lg white-text mr-4"></i></a>
+    </div>
+
+    <div class="footer-copyright text-center py-3">Made with 💖 by
+        <a href="https://twitter.com/tomahock">Tomahock</a>
+
     </div>
 
 </footer>
